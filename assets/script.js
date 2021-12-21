aabb1abe4535e82361daf6821b7a82b2efd192a5
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-21 22:45:31 +1100

    updating js

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,27 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, Do MMM, YYYY"));
 
 //variable
-var textArea = document.querySelector('.plan');
+var currentHour = moment().hour();
+var blockHour = parseInt($('.timeBlock').attr('id'));
+var textArea = $('.plan');
 
-function 
+function changeBgColor() {
+
+    $('.timeBlock').each(function () {
+        if (currentHour < blockHour) {
+            textArea.removeClass('past');
+            textArea.removeClass('present');
+            textArea.addClass('future');
+        } else if (currentHour === blockHour) {
+            textArea.removeClass('past');
+            textArea.addClass('present');
+            textArea.removeClass('future');
+        } else {
+            textArea.addClass('past');
+            textArea.removeClass('present');
+            textArea.removeClass('future');
+        }
+    })
+
+}
+changeBgColor();
