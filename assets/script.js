f1566f46fc4e412abff603c877323e28c15450b7
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-22 13:04:19 +1100

    refactoring html and javascript

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,56 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, Do MMM, YYYY"));
 
 //variable
+var timeBlock = $('.timeBlock');
 var currentHour = moment().hour();
-
 var textArea = $('.plan');
+var saveBtn = $('.saveBtn');
+var clearBtn = $('.clearBtn');
+
+
+timeBlock.each(function () {
+    $(this).children(":first").addClass("hour col-md-1");
+    $(this).children("textarea").addClass("col-md-9 plan")
+    $(this).children("button:first").addClass("btn clearBtn col-md-1 ");
+    $(this).children("button:last").addClass("btn saveBtn col-md-1");
+})
 
 
 function changeBgColor() {
 
     $('.timeBlock').each(function () {
-        var blockHour = parseInt($('.timeBlock').attr('id'));
+        var blockHour = parseInt($(this).attr('id'));
         console.log(blockHour);
         if (currentHour > blockHour) {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
             $(this).addClass('future');
         } else if (currentHour === blockHour) {
-            $(this).removeClass('past');
             $(this).addClass('present');
-            $(this).removeClass('future');
         } else {
             $(this).addClass('past');
-            $(this).removeClass('present');
-            $(this).removeClass('future');
         }
     })
 
 }
 changeBgColor();
+
+saveBtn.click(function () {
+    var textValue = $(this).siblings('textarea').val()
+    var btnHour = $(this).parent().attr('id');
+    console.log(textValue);
+    console.log(btnHour);
+    localStorage.setItem(btnHour, textValue);
+})
+
+clearBtn.click(function () {
+    var btnHour = $(this).parent().attr('id');
+    localStorage.clear(btnHour, textValue);
+})
+
+
+
+
+
+    // give data class on HTML 
+    // var btnHour = $(this).attr('data-hour');
+    // var text = $('textarea[data-tArea=' + btnHour + ']').val()
+    // var text = $(`textarea[data-tArea=${btnHour }]`).val()
